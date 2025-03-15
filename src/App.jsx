16b73b97f2eb8f429482bd8c1b937f43a7938aa3
Devansh Sharma <sharmadevansh007777@gmail.com>
2025-03-15 20:23:47 +0100

    changed dependencies to have tailwind ^4.0

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 function App() {
   return (
     <>
+    <span className=" text-white p-2 block text-center bg-amber-700">Tailwind CSS
+
+    
       <h1 className="my-8 text-center text-5xl font-bold">Hello World</h1>
+      </span>
+
     </>
-  );
+  );    
 }
 
 export default App;
