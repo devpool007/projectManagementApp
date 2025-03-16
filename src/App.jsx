a1238b797cedf4d697997b2cedd37d91eb3737ae
Devansh Sharma <sharmadevansh007777@gmail.com>
2025-03-16 13:50:03 +0100

    corrected the tailwind transition of sidebar with new project component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import ProjectsSidebar from "./components/ProjectsSidebar";
 import NewProject from "./components/NewProject";
+import { useState } from "react";
 
 function App() {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+  function handleToggle() {
+    setIsSidebarOpen(!isSidebarOpen);
+  }
+
   return (
     <main className="h-screen my-8 flex gap-8">
-    <ProjectsSidebar/>
-    <NewProject/>
+    <ProjectsSidebar isOpen={isSidebarOpen} handleToggle={handleToggle}/>
+    <NewProject isSidebarOpen = {isSidebarOpen}/>
     </main>
   );    
 }
