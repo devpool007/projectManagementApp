3488cd59d1a90321197d0a3e0b1c07cef1198db8
devpool007 <sharmadevansh007777@gmail.com>
2025-03-28 12:52:50 +0100

    added Tasks and new tasks components and improved selected projects CSS

---
+++ src/components/NewTasks.jsx
@@ -0,0 +1,6 @@
+export default function NewTasks(){
+    return <div className="flex items-center gap-4">
+        <input type="text" className="w-64 px-2 py-1 rounded-sm" />
+        <button className="text-stone-700 hover:text-stone-200 hover:bg-stone-500 rounded-md p-2">ADD TASK</button>
+    </div>
+} 
